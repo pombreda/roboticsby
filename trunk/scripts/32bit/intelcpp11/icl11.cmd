0ba33b75f6253b72370df4c02cabe4d55ec42cdf
sgerasuto@tut.by <sgerasuto@tut.by@9e829c2e-0fe7-11df-962a-0b01395cd2b2>
2010-02-19 14:06:12 +0000

    Add simple DirectX SDK detection before build started.

--- trunk/scripts/32bit/intelcpp11/icl11.cmd
+++ trunk/scripts/32bit/intelcpp11/icl11.cmd
@@ -1,8 +1,16 @@
-if not exist "%ICPP_COMPILER11%\bin\ia32\iclvars_ia32.bat" goto missing 
+if not exist "%ICPP_COMPILER11%\bin\ia32\iclvars_ia32.bat" goto missing
+if not exist "%DXSDK_DIR%Include\d3d9.h" goto missingdxsdk
 @"%ICPP_COMPILER11%\bin\ia32\iclvars_ia32.bat"
 
 :missing
 @echo Intel C++ 11 not detected
+@pause
+@echo on
+@exit
+goto :eof
+
+:missingdxsdk
+@echo Microsoft DirectX SDK not detected
 @pause
 @echo on
 @exit
--- trunk/scripts/32bit/vs2005/cl2005.cmd
+++ trunk/scripts/32bit/vs2005/cl2005.cmd
@@ -1,8 +1,16 @@
-if not exist "%VS80COMNTOOLS%\vsvars32.bat" goto missing 
+if not exist "%VS80COMNTOOLS%\vsvars32.bat" goto missing
+if not exist "%DXSDK_DIR%Include\d3d9.h" goto missingdxsdk
 @"%VS80COMNTOOLS%\vsvars32.bat"
 
 :missing
 @echo Microsoft Visual Studio 2005 not detected
+@pause
+@echo on
+@exit
+goto :eof
+
+:missingdxsdk
+@echo Microsoft DirectX SDK not detected
 @pause
 @echo on
 @exit

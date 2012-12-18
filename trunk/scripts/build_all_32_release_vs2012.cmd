1a2f54f62f2be56c84ce1370574b25142b30ebb2
sgerasuto%tut.by@gtempaccount.com <sgerasuto%tut.by@gtempaccount.com@9e829c2e-0fe7-11df-962a-0b01395cd2b2>
2012-12-18 12:39:10 +0000

    Sources update.

--- trunk/scripts/build_all_32_release_vs2012.cmd
+++ trunk/scripts/build_all_32_release_vs2012.cmd
@@ -5,7 +5,7 @@
 
 @echo Progress: Build Started using MVS 2012
 @call 32bit\vs2012\cl2012.cmd
-cl /EHsc /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /arch:SSE2 /O1 /analyze ..\src\tigra_joystick_module_test.cpp /link Winmm.lib user32.lib
+cl /EHsc /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /arch:SSE2 /O1 /analyze ..\src\tigger_joystick_keyboard_module_test.cpp /link Winmm.lib user32.lib
 cl /EHsc /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /arch:SSE2 /O1 /analyze ..\src\system_information_detection_module_test.cpp
 cl /EHsc /D "_UNICODE" /D "UNICODE" /arch:SSE2  /D "WIN32" /D "_WINDOWS" /O1 /analyze ..\src\system_information_detection_module_test_win.cpp /link user32.lib gdi32.lib kernel32.lib 
 cl /EHsc /D "_UNICODE" /D "UNICODE" /arch:SSE2 /LD /O1 -I"%DXSDK_DIR%Include" /analyze ..\src\system_information_detection_module\system_information_detection_module.cpp ..\src\system_information_detection_module\gpu_information.cpp ..\src\system_information_detection_module\cpu_information.cpp ..\src\system_information_detection_module\directx_information.cpp /link /LIBPATH:"%DXSDK_DIR%Lib\x86\"  Version.lib dxguid.lib  kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib
